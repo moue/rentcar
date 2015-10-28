38c103570ca8b382a9662984ece2741e5ac10c03
Yuqi Hou <yuqi.hou@gmail.com>
2015-10-28 11:45:55 -0400

    Added ability to OrderBy

--- app/app.js
+++ app/app.js
@@ -10,16 +10,23 @@ angular.module('myApp', [
   'mobile-angular-ui',
   'datePicker'
 ])
-.controller("CarsController", function($scope) {
+.controller("CarsController", ['$scope', '$filter', function($scope,$filter) {
+  var orderBy = $filter('orderBy');
   $scope.cars = [
+    { id:6, type: "Compact", price: 10, pic:"imgs/compact.png"},
     { id:1, type: "Economy", price: 12, pic:"imgs/economy.png" },
     { id:2, type: "Full Size ", price: 15, pic:"imgs/full-size.png" },
     { id:3, type: "Convertible", price: 16, pic:"imgs/convertible.png" },
     { id:4, type: "Minivan", price: 17, pic:"imgs/minivan.png" },
-    { id:5, type: "Luxury", price: 20, pic:"imgs/luxury.png"},
-    { id:6, type: "Compact", price: 10, pic:"imgs/compact.png"}
+    { id:5, type: "Luxury", price: 20, pic:"imgs/luxury.png"}
   ];
-})
+  $scope.reverse='true';
+  $scope.predicate = 'price';
+    $scope.order = function(predicate, reverse) {
+    $scope.cars = orderBy($scope.cars, predicate, reverse);
+  };
+  $scope.order('cars',true);
+}])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
