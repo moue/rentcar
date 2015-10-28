43ca8a1d5053b7826aa8ac57f95e41b6fac70ac3
Yuqi Hou <yuqi.hou@gmail.com>
2015-10-28 04:02:54 -0400

    init

--- app/app.js
+++ app/app.js
@@ -5,8 +5,21 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+  'myApp.view3',
+  'myApp.version',
+  'mobile-angular-ui',
+  'datePicker'
+])
+.controller("CarsController", function($scope) {
+  $scope.cars = [
+    { id:1, type: "Economy", price: 12, pic:"imgs/economy.png" },
+    { id:2, type: "Full Size ", price: 15, pic:"imgs/full-size.png" },
+    { id:3, type: "Convertible", price: 16, pic:"imgs/convertible.png" },
+    { id:4, type: "Minivan", price: 17, pic:"imgs/minivan.png" },
+    { id:5, type: "Luxury", price: 20, pic:"imgs/luxury.png"},
+    { id:6, type: "Compact", price: 10, pic:"imgs/compact.png"}
+  ];
+})
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
---
+++ app/view2/notes.md
@@ -0,0 +1,10 @@
+# Start with Angular Seed
+https://github.com/angular/angular-seed
+
+git clone --depth=1 https://github.com/angular/angular-seed.git <your-project-name>
+
+# Install Angular UI Mobile
+bower install --save mobile-angular-ui
+
+# source
+http://pictures.dealer.com/b/barrycountycars/0755/678e107f4c1b83e2dbe83f31fc545d04x.jpg
